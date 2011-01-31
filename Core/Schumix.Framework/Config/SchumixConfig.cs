284826bc15cec6c7c0c4525bd0940f7ff04dbf17
Megax <megax@yeahunter.hu>
2011-01-31 21:50:05 +0100

    * Fixálások.

--- Core/Schumix.Framework/Config/SchumixConfig.cs
+++ Core/Schumix.Framework/Config/SchumixConfig.cs
@@ -23,7 +23,7 @@ namespace Schumix.Framework.Config
 {
 	public class Verzio
 	{
-		private readonly static string _SchumixVerzio = "1.5.5";
+		private readonly static string _SchumixVerzio = "1.5.6";
 		public static string SchumixVerzio
 		{
 			get
