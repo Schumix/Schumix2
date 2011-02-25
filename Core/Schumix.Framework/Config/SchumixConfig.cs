bc88d0371741b93d540f2c24277d0be7234f7eaf
Megax <megax@yeahunter.hu>
2011-02-25 18:13:32 +0100

    * Url encode hozzáadva a kódhoz.

--- Core/Schumix.Framework/Config/SchumixConfig.cs
+++ Core/Schumix.Framework/Config/SchumixConfig.cs
@@ -23,7 +23,7 @@ namespace Schumix.Framework.Config
 {
 	public sealed class Verzio
 	{
-		private readonly static string _SchumixVerzio = "2.0.7";
+		private readonly static string _SchumixVerzio = "2.0.8";
 		public static string SchumixVerzio
 		{
 			get
