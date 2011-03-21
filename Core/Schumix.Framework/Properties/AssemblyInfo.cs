4136cfeb0f9eb29aeddb6de9f1f05e7d5b2362cb
megax <megax@megaxx.info>
2011-03-21 20:55:09 +0100

    * Fix.

--- Core/Schumix.Framework/Properties/AssemblyInfo.cs
+++ Core/Schumix.Framework/Properties/AssemblyInfo.cs
@@ -19,7 +19,6 @@
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using Schumix.Framework.Config;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
