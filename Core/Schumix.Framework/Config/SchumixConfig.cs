6cd369b334ed2f38f775f877eff306edace25998
megax <megax@megaxx.info>
2011-04-15 17:44:12 +0200

    * Config fájl szerkezete megigazítva.

--- Core/Schumix.Framework/Config/SchumixConfig.cs
+++ Core/Schumix.Framework/Config/SchumixConfig.cs
@@ -23,6 +23,6 @@ namespace Schumix.Framework.Config
 {
 	public sealed class Verzio
 	{
-		public static string SchumixVerzio { get { return "2.8.1"; } }
+		public static string SchumixVerzio { get { return "2.8.2"; } }
 	}
 }
