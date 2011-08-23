176808f700e3bb2df20542dae95e7449e083078c
Megax <megax@megaxx.info>
2011-08-23 12:25:56 +0200

    * Adatbazis frissitve lett. A kod 100%-ig tamogatja a masik nyelvezetet. Jelenleg meg a functios-ban van egy kis magyar de hamarosan orvosolva lesz. Mindezert koszonet Alenah-nak.

---
+++ Components/Schumix.LuaEngine/LuaFunctionAttribute.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * This file is part of Schumix.
+ * 
+ * Copyright (C) 2010-2011 Twl
+ * Copyright (C) 2010-2011 Megax <http://www.megaxx.info/>
+ * 
+ * Schumix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Schumix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Schumix.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace Schumix.LuaEngine
+{
+    /// <summary>
+    /// Attribute used to mark functions which are exposed to the Lua VM.
+    /// </summary>
+    public sealed class LuaFunctionAttribute : Attribute
+    {
+        #region Private Members
+
+        private readonly string _functionName;
+        private readonly string _functionDoc;
+        private readonly string[] _functionParameters;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the name of the Lua function.
+        /// </summary>
+        public string FunctionName
+        {
+            get
+            {
+                return _functionName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the function documentation.
+        /// </summary>
+        public string FunctionDocumentation
+        {
+            get
+            {
+                return _functionDoc;
+            }
+        }
+
+        /// <summary>
+        /// Gets the function parameters.
+        /// </summary>
+        public string[] FunctionParameters
+        {
+            get
+            {
+                return _functionParameters;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new attribute marking the function to be registered by the Lua VM.
+        /// </summary>
+        /// <param name="functionName">Name of the function. Will be used in Lua (does not have to be the same as the C# function name).</param>
+        /// <param name="functionDoc">Function documentation</param>
+        /// <param name="parameterDocs">Parameter documentation</param>
+        public LuaFunctionAttribute(string functionName, string functionDoc, params string[] parameterDocs)
+        {
+            _functionName = functionName;
+            _functionDoc = functionDoc;
+            _functionParameters = parameterDocs;
+        }
+
+        /// <summary>
+        /// Creates a new attribute marking the function to be registered by the Lua VM.
+        /// </summary>
+        /// <param name="functionName">Name of the function. Will be used in Lua (does not have to be the same as the C# function name).</param>
+        /// <param name="functionDoc">Function documentation</param>
+        public LuaFunctionAttribute(string functionName, string functionDoc)
+        {
+            _functionName = functionName;
+            _functionDoc = functionDoc;
+        }
+
+        #endregion
+    }
+}
---
+++ Components/Schumix.LuaEngine/LuaFunctions.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * This file is part of Schumix.
+ * 
+ * Copyright (C) 2010-2011 Twl
+ * Copyright (C) 2010-2011 Megax <http://www.megaxx.info/>
+ * 
+ * Schumix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Schumix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Schumix.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
+using LuaInterface;
+
+namespace Schumix.LuaEngine
+{
+    /// <summary>
+    /// Class containing most of the exported Lua functions.
+    /// It as also a wrapper to the Main API.
+    /// </summary>
+    public sealed class LuaFunctions
+    {
+        //private readonly Connection _connection;
+        private readonly Lua _lua;
+        //private readonly List<PublicMessageEventHandler> _registeredOnPM = new List<PublicMessageEventHandler>();
+
+        #region Properites
+
+        /// <summary>
+        /// Events registered by Lua on Public Message.
+        /// </summary>
+        /*public IEnumerable<PublicMessageEventHandler> RegisteredOnPM
+        {
+            get
+            {
+                return _registeredOnPM;
+            }
+        }*/
+
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance of <c>LuaFunctions</c>
+        /// </summary>
+        /// <param name="vm">Lua VM</param>
+        /// <param name="conn">IRC connection</param>
+        public LuaFunctions(ref Lua vm/*, ref Connection conn*/)
+        {
+            //_connection = conn;
+            _lua = vm;
+        }
+
+        /// <summary>
+        /// Registers a function hook.
+        /// </summary>
+        /// <param name="eventName">Event to listen to.</param>
+        /// <param name="luaName">Lua function to call.</param>
+        [LuaFunction("RegisterHook", "Registers a function hook.")]
+        public void RegisterFunctionHook(string eventName, string luaName)
+        {
+            if(eventName == "OnPublicMessage")
+            {
+                var func = _lua.GetFunction(typeof (PublicMessageEventHandler), luaName);
+
+                if (func == null)
+                    return;
+
+                var handler = func as PublicMessageEventHandler;
+
+                //_connection.Listener.OnPublic += handler;
+                _registeredOnPM.Add(handler);
+            }
+            
+        }
+
+        /// <summary>
+        /// Sends a message to the IRC server.
+        /// </summary>
+        /// <param name="chan">Channel to send to.</param>
+        /// <param name="message">Message.</param>
+        [LuaFunction("SendMsg", "Sends a message to the IRC server.")]
+        public void SendMessage(string chan, string message)
+        {
+            _connection.Sender.PublicMessage(chan, message);
+        }
+    }
+}
