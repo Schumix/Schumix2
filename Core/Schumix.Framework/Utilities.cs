fd8c94bae912d3ed2f23a58644e1d4dab3e2c2fe
Megax <megax@megaxx.info>
2011-09-04 14:33:50 +0200

    * CTCP üzenetek egyrészére kész a válasz. Reply kódok használhatóak inéttől.

--- Core/Schumix.Framework/Utilities.cs
+++ Core/Schumix.Framework/Utilities.cs
@@ -244,6 +244,110 @@ namespace Schumix.Framework
 			return Platform;
 		}
 
+		/// <summary>
+		/// Returns the name of the operating system running on this computer.
+		/// </summary>
+		/// <returns>A string containing the the operating system name.</returns>
+		public string GetOSName()
+		{
+			var Info = Environment.OSVersion;
+			string Name = string.Empty;
+			
+			switch(Info.Platform)
+			{
+				case PlatformID.Win32Windows:
+				{
+					switch(Info.Version.Minor)
+					{
+						case 0:
+						{
+							Name = "Windows 95";
+							break;
+						}
+						case 10:
+						{
+							if(Info.Version.Revision.ToString() == "2222A")
+								Name = "Windows 98 Second Edition";
+							else
+								Name = "Windows 98";
+
+							break;
+						}
+						case 90:
+						{
+							Name = "Windows Me";
+							break;
+						}
+					}
+
+					break;
+				}
+				case PlatformID.Win32NT:
+				{
+					switch(Info.Version.Major)
+					{
+						case 3:
+						{
+							Name = "Windows NT 3.51";
+							break;
+						}
+						case 4:
+						{
+							Name = "Windows NT 4.0";
+							break;
+						}
+						case 5:
+						{
+							if(Info.Version.Minor == 0)
+								Name = "Windows 2000";
+							else if(Info.Version.Minor == 1)
+								Name = "Windows XP";
+							else if(Info.Version.Minor == 2)
+								Name = "Windows Server 2003";
+							break;
+						}
+						case 6:
+						{
+							if(Info.Version.Minor == 0)
+								Name = "Windows Vista";
+							else if(Info.Version.Minor == 1)
+								Name = "Windows 7";
+							break;
+						}
+					}
+
+					break;
+				}
+				case PlatformID.WinCE:
+				{
+					Name = "Windows CE";
+					break;
+				}
+				case PlatformID.Unix:
+				{
+					Name = "Linux " + Info.Version;
+					break;
+				}
+				case PlatformID.MacOSX:
+				{
+					Name = "MacOSX";
+					break;
+				}
+				case PlatformID.Xbox:
+				{
+					Name = "Xbox";
+					break;
+				}
+				default:
+				{
+					Name = "Unknown";
+					break;
+				}
+			}
+
+			return Name;
+		}
+
 		public string GetVersion()
 		{
 			return Schumix.Framework.Config.Consts.SchumixVersion;
