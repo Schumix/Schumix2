ce420937a6165f9d2987cea0fe51d166dbe79cdc
Megax <megax@megaxx.info>
2012-04-23 15:54:09 +0200

    * Javítva a log fájl generálása. Ezentúl nem új sorral kezdi hanem az időponttal.

--- Core/Schumix.Framework/Localization/LocalizationConsole.cs
+++ Core/Schumix.Framework/Localization/LocalizationConsole.cs
@@ -264,6 +264,15 @@ namespace Schumix.Framework.Localization
 				case "Text":
 				{
 					if(Locale == "huHU")
+						return "Indulási időpont: [{0}. {1}. {2}. {3}:{4}:{5}]\n";
+					else if(Locale == "enUS")
+						return "Started time: [{0}. {1}. {2}. {3}:{4}:{5}]\n";
+					else
+						return "Started time: [{0}. {1}. {2}. {3}:{4}:{5}]\n";
+				}
+				case "Text2":
+				{
+					if(Locale == "huHU")
 						return "\nIndulási időpont: [{0}. {1}. {2}. {3}:{4}:{5}]\n";
 					else if(Locale == "enUS")
 						return "\nStarted time: [{0}. {1}. {2}. {3}:{4}:{5}]\n";
@@ -2219,6 +2228,24 @@ namespace Schumix.Framework.Localization
 						return "One of the summix has been shuted down, it will be restarted.";
 					else
 						return "One of the summix has been shuted down, it will be restarted.";
+				}
+				default:
+					return string.Empty;
+			}
+		}
+
+		public string CommandInfo(string Name)
+		{
+			switch(Name)
+			{
+				case "Text":
+				{
+					if(Locale == "huHU")
+						return "CommandInfo sikeresen elindult.";
+					else if(Locale == "enUS")
+						return "Successfully started the CommandInfo.";
+					else
+						return "Successfully started the CommandInfo.";
 				}
 				default:
 					return string.Empty;
