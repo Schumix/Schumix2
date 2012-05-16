42020a274b1c9a4a192c36c0faac8c6be7a9a73b
Megax <megax@yeahunter.hu>
2012-05-16 18:06:01 +0200

    * Fixalva az egyik sql frissites.

--- Sql/Updates/3.7.7_base.sql
+++ Sql/Updates/3.7.7_base.sql
@@ -4,6 +4,6 @@ INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("huHU", 
 INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("huHU", "NoIgnoreMyNick", "Nem tiltható le a saját neved!");
 
 -- enUS
-INSERT INTO `localized_console_warning` (`Language`, `Command`, `Text`) VALUES ("huHU", "NoIgnoreMasterChannel", "The master channel shouldn't be ignored!");
-INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("huHU", "NoIgnoreMasterChannel", "The master channel shouldn't be ignored!");
-INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("huHU", "NoIgnoreMyNick", "Your own name shouldn't be blocked!");
+INSERT INTO `localized_console_warning` (`Language`, `Command`, `Text`) VALUES ("enUS", "NoIgnoreMasterChannel", "The master channel shouldn't be ignored!");
+INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("enUS", "NoIgnoreMasterChannel", "The master channel shouldn't be ignored!");
+INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("enUS", "NoIgnoreMyNick", "Your own name shouldn't be blocked!");
