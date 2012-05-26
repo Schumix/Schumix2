12f2f78564d4f194de59229bc60e5014c2e9ab18
Megax <megax@megaxx.info>
2012-05-26 13:03:55 +0200

    * Windows-os leallitas javitva a botban.

--- Applications/Schumix/Main.cs
+++ Applications/Schumix/Main.cs
@@ -22,8 +22,6 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using System.Globalization;
-using Mono.Unix;
-using Mono.Unix.Native;
 using Schumix.Irc;
 using Schumix.Updater;
 using Schumix.Framework;
@@ -50,11 +48,13 @@ namespace Schumix
 		/// </summary>
 		private static readonly Utilities sUtilities = Singleton<Utilities>.Instance;
 		private static readonly Runtime sRuntime = Singleton<Runtime>.Instance;
+		private static readonly Windows sWindows = Singleton<Windows>.Instance;
 		/// <summary>
 		///     Hozzáférést biztosít singleton-on keresztül a megadott class-hoz.
 		///     Üzenet küldés az irc szerver felé.
 		/// </summary>
 		private static readonly Sender sSender = Singleton<Sender>.Instance;
+		private static readonly Linux sLinux = Singleton<Linux>.Instance;
 
 		/// <summary>
 		///     A Main függvény. Itt indul el a program.
@@ -204,17 +204,10 @@ namespace Schumix
 
 			new SchumixBot();
 
-			if(sUtilities.GetPlatformType() == PlatformType.Linux)
-				StartHandler();
-			else
-			{
-				System.Console.CancelKeyPress += (sender, e) =>
-				{
-					SchumixBase.Quit();
-					sSender.Quit("Daemon killed.");
-					Thread.Sleep(5*1000);
-				};
-			}
+			if(sUtilities.GetPlatformType() == PlatformType.Windows)
+				sWindows.Init();
+			else if(sUtilities.GetPlatformType() == PlatformType.Linux)
+				sLinux.Init();
 
 			AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
 			{
@@ -248,23 +241,6 @@ namespace Schumix
 			System.Console.WriteLine("\t--server-password=<pass>\tSet password.");
 			System.Console.WriteLine("\t--server-identify=Value\t\tSet identify.");
 			System.Console.WriteLine("\t--server-configs=Value\t\tSend Schumix's parameters at all.");
-		}
-
-		private static void StartHandler()
-		{
-			new Thread(TerminateHandler).Start();
-		}
-
-		private static void TerminateHandler()
-		{
-			Log.Notice("Main", "Initializing Handler for SIGINT");
-			var signal = new UnixSignal(Signum.SIGINT);
-			signal.WaitOne();
-
-			Log.Notice("Main", "Handler Terminated");
-			SchumixBase.Quit();
-			sSender.Quit("Daemon killed.");
-			Thread.Sleep(5*1000);
 		}
 	}
 }
