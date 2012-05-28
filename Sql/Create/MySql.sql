6f051c11c86076633d839c162dd04e6b63d8e22f
Invisible <yeahunter@gmail.com>
2012-05-28 17:21:49 +0200

    GameAddon: MaffiaGame: Statisztika megirva ;) Signed-off-by: Invisible <yeahunter@gmail.com>

--- Sql/Create/MySql.sql
+++ Sql/Create/MySql.sql
@@ -333,3 +333,16 @@ CREATE TABLE `ignore_irc_commands` (
   `Command` varchar(30) NOT NULL default '',
   PRIMARY KEY  (`Id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for maffiagame
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `maffiagame` (
+  `ID` int(4) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Kulcs',
+  `Game` int(5) NOT NULL COMMENT 'Hanyadik Jatek',
+  `Nev` varchar(25) NOT NULL COMMENT 'A jatekos neve',
+  `Survivor` int(3) NOT NULL DEFAULT '1' COMMENT 'A jatekos tul elte-e az adott jatszmat',
+  `Job` int(3) NOT NULL COMMENT 'A jatekos szerepe',
+  `Active` int(3) NOT NULL DEFAULT '1' COMMENT 'Megy-e meg az adott jatek',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
