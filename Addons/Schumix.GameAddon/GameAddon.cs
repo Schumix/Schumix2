04ea362c26fac218b3e212dd05f6f593e919c1be
Megax <megax@yeahunter.hu>
2012-05-29 17:33:39 +0200

    * Jatek addonban fixalva a befejezesi uzenet kiirasa. Igy most egy helyrol irodik ki es jobban is mukodik igy majd az angolositasa a jateknak.

--- Addons/Schumix.GameAddon/GameAddon.cs
+++ Addons/Schumix.GameAddon/GameAddon.cs
@@ -368,12 +368,7 @@ namespace Schumix.GameAddon
 								{
 									MaffiaList[channel].RemoveRanks();
 									sSendMessage.SendCMPrivmsg(sIRCMessage.Channel, "A játék befejeződött.");
-
-									if(MaffiaList[channel].GetPlayers() < 8)
-										sSendMessage.SendCMPrivmsg(sIRCMessage.Channel, "*** A gyilkos 4{0} volt, a nyomozó 4{1}, az orvos pedig nem volt. Mindenki más hétköznapi civil volt.", MaffiaList[channel].GetKiller(), MaffiaList[channel].GetDetective());
-									else
-										sSendMessage.SendCMPrivmsg(sIRCMessage.Channel, "*** A gyilkos 4{0} volt, a nyomozó 4{1}, az orvos pedig 4{2}. Mindenki más hétköznapi civil volt.", MaffiaList[channel].GetKiller(), MaffiaList[channel].GetDetective(), MaffiaList[channel].GetDoctor());
-
+									MaffiaList[channel].EndText();
 									MaffiaList[channel].StopThread();
 								}
 								else
