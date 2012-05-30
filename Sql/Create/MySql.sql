cf46601b07ed6daad2cfaa2db4b7a07a590855d0
Megax <megax@yeahunter.hu>
2012-05-30 16:46:29 +0200

    * Fix.

--- Sql/Create/MySql.sql
+++ Sql/Create/MySql.sql
@@ -340,7 +340,7 @@ CREATE TABLE `ignore_irc_commands` (
 CREATE TABLE `maffiagame` (
   `Id` int(4) unsigned NOT NULL AUTO_INCREMENT COMMENT 'key',
   `Game` int(5) NOT NULL COMMENT 'th',
-  `Nev` varchar(25) NOT NULL COMMENT 'Player name',
+  `Name` varchar(25) NOT NULL COMMENT 'Player name',
   `Survivor` int(3) NOT NULL DEFAULT '1' COMMENT 'The player has survived?',
   `Job` int(3) NOT NULL COMMENT 'What the players job?',
   `Active` int(3) NOT NULL DEFAULT '1' COMMENT 'Is active game?',
