239d5a12c3493cdc5438233c2ac235197a38ea53
Megax <megax@megaxx.info>
2012-05-30 16:46:29 +0200

    * Fix.

--- Sql/Updates/3.7.9_1.sql
+++ Sql/Updates/3.7.9_1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `maffiagame` (
   `ID` int(4) unsigned NOT NULL AUTO_INCREMENT COMMENT 'key',
   `Game` int(5) NOT NULL COMMENT 'th',
-  `Nev` varchar(25) NOT NULL COMMENT 'Player name',
+  `Name` varchar(25) NOT NULL COMMENT 'Player name',
   `Survivor` int(3) NOT NULL DEFAULT '1' COMMENT 'The player has survived?',
   `Job` int(3) NOT NULL COMMENT 'What the players job?',
   `Active` int(3) NOT NULL DEFAULT '1' COMMENT 'Is active game?',
