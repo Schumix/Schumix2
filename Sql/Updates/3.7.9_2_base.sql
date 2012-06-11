49b539077281413f16be0e48883343f2fbcdfb3c
Megax <megax@yeahunter.hu>
2012-06-11 19:58:07 +0200

    * Par fix.

--- Sql/Updates/3.7.9_2_base.sql
+++ Sql/Updates/3.7.9_2_base.sql
@@ -1,8 +1,8 @@
 -- huHU
 UPDATE `localized_command` SET Text = "{0}: 3betöltve.\n{0}: 8letiltva." WHERE Language = 'huHU' AND Command = 'plugin';
 UPDATE `localized_console_command` SET Text = "{0}: betöltve.\n{0}: letiltva." WHERE Language = 'huHU' AND Command = 'plugin';
-UPDATE `localized_command_help` SET Text = "Lehetõvé teszi egyes adatok kivételként való kezelését.\nIgnore parancsok: irc | command | channel | addon" WHERE Language = 'huHU' AND Command = 'ignore';
-UPDATE `localized_console_command_help` SET Text = "Lehetõvé teszi egyes adatok kivételként való kezelését.\nIgnore parancsok: irc | command | channel | addon" WHERE Language = 'huHU' AND Command = 'ignore';
+UPDATE `localized_command_help` SET Text = "Lehetõvé teszi egyes adatok kivételként való kezelését.\nIgnore parancsok: irc | command | channel | nick | addon" WHERE Language = 'huHU' AND Command = 'ignore';
+UPDATE `localized_console_command_help` SET Text = "Lehetõvé teszi egyes adatok kivételként való kezelését.\nIgnore parancsok: irc | command | channel | nick | addon" WHERE Language = 'huHU' AND Command = 'ignore';
 INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("huHU", "ignore/addon/add", "Már szerepel az ignore listán!\nAz addon sikeresen hozzáadásra került.");
 INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("huHU", "ignore/addon/remove", "Nem szerepel az ignore listán!\nAz addon sikeresen el lett távolítva.");
 INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("huHU", "ignore/addon/search", "Szerepel az ignore listán!\nNem szerepel az ignore listán!");
@@ -21,8 +21,8 @@ INSERT INTO `localized_console_command_help` (`Language`, `Command`, `Text`) VAL
 -- enUS
 UPDATE `localized_command` SET Text = "{0}: 3loaded.\n{0}: 8ignored." WHERE Language = 'enUS' AND Command = 'plugin';
 UPDATE `localized_console_command` SET Text = "{0}: loaded.\n{0}: ignored." WHERE Language = 'enUS' AND Command = 'plugin';
-UPDATE `localized_command_help` SET Text = "With it you can treat the data like an expection.\nIgnore commands: irc | command | channel | addon" WHERE Language = 'enUS' AND Command = 'ignore';
-UPDATE `localized_console_command_help` SET Text = "With it you can treat the data like an expection.\nIgnore commands: irc | command | channel | addon" WHERE Language = 'enUS' AND Command = 'ignore';
+UPDATE `localized_command_help` SET Text = "With it you can treat the data like an expection.\nIgnore commands: irc | command | channel | nick | addon" WHERE Language = 'enUS' AND Command = 'ignore';
+UPDATE `localized_console_command_help` SET Text = "With it you can treat the data like an expection.\nIgnore commands: irc | command | channel | nick | addon" WHERE Language = 'enUS' AND Command = 'ignore';
 INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("enUS", "ignore/addon/add", "Already exist on the ignore list!\nSuccesfuly added.");
 INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("enUS", "ignore/addon/remove", "Not on the ignore list!\nSuccesfuly removed.");
 INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("enUS", "ignore/addon/search", "Already exist on the ignore list!\nNot on the ignore list!");
