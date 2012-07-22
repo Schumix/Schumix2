51e9522719a8fcfd391baca102b983a9fa51789b
Megax <megax@yeahunter.hu>
2012-07-22 12:10:22 +0200

    * Hoppá :D

--- Core/Schumix.Framework/Config/DefaultConfig.cs
+++ Core/Schumix.Framework/Config/DefaultConfig.cs
@@ -5,16 +5,16 @@
  * Copyright (C) 2012 Jackneill
  * 
  * Schumix is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General protected License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * Schumix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General protected License for more details.
+ * GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General protected License
+ * You should have received a copy of the GNU General Public License
  * along with Schumix.  If not, see <http://www.gnu.org/licenses/>.
  */
 
