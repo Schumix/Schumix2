8f8cdadd654e3562f0b8bc45be075a708b3026ec
Megax <megax@yeahunter.hu>
2012-07-26 20:29:23 +0200

    * Ezt nem akartam így feltenni =/

--- Core/Schumix.Framework/Config/YamlConfig.cs
+++ Core/Schumix.Framework/Config/YamlConfig.cs
@@ -128,14 +128,13 @@ namespace Schumix.Framework.Config
 						nodes2.Add("Update",       CreateUpdateMap((!schumixmap.IsNull() && schumixmap.ContainsKey(new YamlScalarNode("Update"))) ? ((YamlMappingNode)schumixmap[new YamlScalarNode("Update")]).Children : (Dictionary<YamlNode, YamlNode>)null));
 						nodes.Add("Schumix", nodes2);
 
-						Console.WriteLine(ToString(nodes.Children));
-						/*sUtilities.CreateFile(filename);
+						sUtilities.CreateFile(filename);
 						var file = new StreamWriter(filename, true) { AutoFlush = true };
 						file.Write(ToString(nodes.Children));
 						file.Close();
 
 						if(File.Exists(filename2))
-							File.Delete(filename2);*/
+							File.Delete(filename2);
 
 						Log.Success("YamlConfig", sLConsole.Config("Text7"));
 					}
