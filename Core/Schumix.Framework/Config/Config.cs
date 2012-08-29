037720fc42b056edcbf7b07a506982933f2620bc
Megax <megax@yeahunter.hu>
2012-08-29 14:42:19 +0200

    * Message visszahelyezve.

--- Core/Schumix.Framework/Config/Config.cs
+++ Core/Schumix.Framework/Config/Config.cs
@@ -71,7 +71,7 @@ namespace Schumix.Framework.Config
 			{
 				new LogConfig(d_logfilename, 3, d_logdirectory, d_irclogdirectory, d_irclog);
 				Log.Initialize(d_logfilename);
-				Log.Error("Config", sLConsole.Exception("Error"), e);
+				Log.Error("Config", sLConsole.Exception("Error"), e.Message);
 			}
 		}
 
