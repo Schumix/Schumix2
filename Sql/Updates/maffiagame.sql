877ea4e5f1e230e2c247051feff5559df12a278e
Megax <megax@yeahunter.hu>
2012-12-20 11:36:20 +0100

    * Commands.cs nyelvezete áthelyezve sql-be.

---
+++ Sql/Updates/maffiagame.sql
@@ -0,0 +1,5 @@
+-- huHU
+INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("huHU", "game/start", "Ez nem csatorna! Ne pm-ben írj!\nNincs megadva a játék neve!\nFut már játék!\nNincs ilyen játék!");
+INSERT INTO `localized_command` (`Language`, `Command`, `Text`) VALUES ("huHU", "game/start/maffiagame", "{0}: Te már játékban vagy itt: {1}");
+
+-- enUS
