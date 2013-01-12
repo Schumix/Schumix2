8eb5b8e4d50da60f433f19e45005ae28adbf49d8
Megax <megax@yeahunter.hu>
2013-01-12 12:59:34 +0100

    * Szóköz lecserélve tab-ra.

--- Core/Schumix.Framework/Exceptions/SchumixException.cs
+++ Core/Schumix.Framework/Exceptions/SchumixException.cs
@@ -23,63 +23,64 @@ using System.Runtime.Serialization;
 
 namespace Schumix.Framework.Exceptions
 {
-   public sealed class SchumixException : Exception
-   {
-      /// <summary>
-      /// Initializes a new instance.
-      /// </summary>
-      public SchumixException()
-      {
-      }
+	[Serializable]
+	public sealed class SchumixException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance.
+		/// </summary>
+		public SchumixException()
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with a specified error message.
-      /// </summary>
-      /// <param name="message">The message that describes the error.</param>
-      public SchumixException(string message) : base(message)
-      {
-      }
+		/// <summary>
+		/// Initializes a new instance with a specified error message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public SchumixException(string message) : base(message)
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with a reference to the inner 
-      /// exception that is the cause of this exception.
-      /// </summary>
-      /// <param name="innerException">
-      /// The exception that is the cause of the current exception, 
-      /// or a null reference if no inner exception is specified.
-      /// </param>
-      public SchumixException(Exception innerException) : base(null, innerException)
-      {
-      }
+		/// <summary>
+		/// Initializes a new instance with a reference to the inner 
+		/// exception that is the cause of this exception.
+		/// </summary>
+		/// <param name="innerException">
+		/// The exception that is the cause of the current exception, 
+		/// or a null reference if no inner exception is specified.
+		/// </param>
+		public SchumixException(Exception innerException) : base(null, innerException)
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with a specified error message and a 
-      /// reference to the inner exception that is the cause of this exception.
-      /// </summary>
-      /// <param name="message">The message that describes the error.</param>
-      /// <param name="innerException">
-      /// The exception that is the cause of the current exception, 
-      /// or a null reference if no inner exception is specified.
-      /// </param>
-      public SchumixException(string message, Exception innerException) : base(message, innerException)
-      {
-      }
+		/// <summary>
+		/// Initializes a new instance with a specified error message and a 
+		/// reference to the inner exception that is the cause of this exception.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		/// <param name="innerException">
+		/// The exception that is the cause of the current exception, 
+		/// or a null reference if no inner exception is specified.
+		/// </param>
+		public SchumixException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with serialized data.
-      /// </summary>
-      /// <param name="info">
-      /// The <see cref="System.Runtime.Serialization.SerializationInfo"/> that holds the 
-      /// serialized object data about the exception being thrown.
-      /// </param>
-      /// <param name="context">
-      /// The <see cref="System.Runtime.Serialization.StreamingContext"/> that contains 
-      /// contextual information about the source or destination.
-      /// </param>
-      /// <exception cref="System.ArgumentNullException">The info parameter is null.</exception>
-      /// <exception cref="System.Runtime.Serialization.SerializationException">The class name is null or System.Exception.HResult is zero (0).</exception>
-      public SchumixException(SerializationInfo info, StreamingContext context) : base(info, context)
-      {
-      }
-   }
+		/// <summary>
+		/// Initializes a new instance with serialized data.
+		/// </summary>
+		/// <param name="info">
+		/// The <see cref="System.Runtime.Serialization.SerializationInfo"/> that holds the 
+		/// serialized object data about the exception being thrown.
+		/// </param>
+		/// <param name="context">
+		/// The <see cref="System.Runtime.Serialization.StreamingContext"/> that contains 
+		/// contextual information about the source or destination.
+		/// </param>
+		/// <exception cref="System.ArgumentNullException">The info parameter is null.</exception>
+		/// <exception cref="System.Runtime.Serialization.SerializationException">The class name is null or System.Exception.HResult is zero (0).</exception>
+		public SchumixException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
 }
--- Core/Schumix.Framework/Exceptions/SingletonException.cs
+++ Core/Schumix.Framework/Exceptions/SingletonException.cs
@@ -23,70 +23,70 @@ using System.Runtime.Serialization;
 
 namespace Schumix.Framework.Exceptions
 {
-   /// <summary>
-   /// Represents errors that occur while creating a singleton.
-   /// </summary>
-   /// <remarks>
-   /// http://msdn.microsoft.com/en-us/library/ms229064(VS.80).aspx
-   /// </remarks>
-   [Serializable]
-   public sealed class SingletonException : Exception
-   {
-      /// <summary>
-      /// Initializes a new instance.
-      /// </summary>
-      public SingletonException()
-      {
-      }
+	/// <summary>
+	/// Represents errors that occur while creating a singleton.
+	/// </summary>
+	/// <remarks>
+	/// http://msdn.microsoft.com/en-us/library/ms229064(VS.80).aspx
+	/// </remarks>
+	[Serializable]
+	public sealed class SingletonException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance.
+		/// </summary>
+		public SingletonException()
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with a specified error message.
-      /// </summary>
-      /// <param name="message">The message that describes the error.</param>
-      public SingletonException(string message) : base(message)
-      {
-      }
+		/// <summary>
+		/// Initializes a new instance with a specified error message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public SingletonException(string message) : base(message)
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with a reference to the inner 
-      /// exception that is the cause of this exception.
-      /// </summary>
-      /// <param name="innerException">
-      /// The exception that is the cause of the current exception, 
-      /// or a null reference if no inner exception is specified.
-      /// </param>
-      public SingletonException(Exception innerException) : base(null, innerException)
-      {
-      }
+		/// <summary>
+		/// Initializes a new instance with a reference to the inner 
+		/// exception that is the cause of this exception.
+		/// </summary>
+		/// <param name="innerException">
+		/// The exception that is the cause of the current exception, 
+		/// or a null reference if no inner exception is specified.
+		/// </param>
+		public SingletonException(Exception innerException) : base(null, innerException)
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with a specified error message and a 
-      /// reference to the inner exception that is the cause of this exception.
-      /// </summary>
-      /// <param name="message">The message that describes the error.</param>
-      /// <param name="innerException">
-      /// The exception that is the cause of the current exception, 
-      /// or a null reference if no inner exception is specified.
-      /// </param>
-      public SingletonException(string message, Exception innerException) : base(message, innerException)
-      {
-      }
+		/// <summary>
+		/// Initializes a new instance with a specified error message and a 
+		/// reference to the inner exception that is the cause of this exception.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		/// <param name="innerException">
+		/// The exception that is the cause of the current exception, 
+		/// or a null reference if no inner exception is specified.
+		/// </param>
+		public SingletonException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
 
-      /// <summary>
-      /// Initializes a new instance with serialized data.
-      /// </summary>
-      /// <param name="info">
-      /// The <see cref="System.Runtime.Serialization.SerializationInfo"/> that holds the 
-      /// serialized object data about the exception being thrown.
-      /// </param>
-      /// <param name="context">
-      /// The <see cref="System.Runtime.Serialization.StreamingContext"/> that contains 
-      /// contextual information about the source or destination.
-      /// </param>
-      /// <exception cref="System.ArgumentNullException">The info parameter is null.</exception>
-      /// <exception cref="System.Runtime.Serialization.SerializationException">The class name is null or System.Exception.HResult is zero (0).</exception>
-      public SingletonException(SerializationInfo info, StreamingContext context) : base(info, context)
-      {
-      }
-   }
+		/// <summary>
+		/// Initializes a new instance with serialized data.
+		/// </summary>
+		/// <param name="info">
+		/// The <see cref="System.Runtime.Serialization.SerializationInfo"/> that holds the 
+		/// serialized object data about the exception being thrown.
+		/// </param>
+		/// <param name="context">
+		/// The <see cref="System.Runtime.Serialization.StreamingContext"/> that contains 
+		/// contextual information about the source or destination.
+		/// </param>
+		/// <exception cref="System.ArgumentNullException">The info parameter is null.</exception>
+		/// <exception cref="System.Runtime.Serialization.SerializationException">The class name is null or System.Exception.HResult is zero (0).</exception>
+		public SingletonException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
 }
