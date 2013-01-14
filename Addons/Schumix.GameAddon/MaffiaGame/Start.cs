7798704fa152b9cf855b96b62d72f94bff56cc86
Megax <megax@yeahunter.hu>
2013-01-14 19:20:06 +0100

    * Így szebb :)

--- Addons/Schumix.GameAddon/MaffiaGame/Start.cs
+++ Addons/Schumix.GameAddon/MaffiaGame/Start.cs
@@ -21,6 +21,7 @@
 using System;
 using System.Threading;
 using System.Collections.Generic;
+using Schumix.Irc.Commands;
 using Schumix.Framework;
 using Schumix.Framework.Config;
 
@@ -88,7 +89,7 @@ namespace Schumix.GameAddon.MaffiaGames
 						list.TryGetValue(number, out name);
 						_killerlist.Add(name.ToLower(), name);
 
-						if(Adminflag(name.ToLower()) == 2)
+						if((AdminFlag)Adminflag(name.ToLower()) == AdminFlag.Administrator)
 							_playerflist.Add(name.ToLower(), new Player(Rank.Killer, true));
 						else
 							_playerflist.Add(name.ToLower(), new Player(Rank.Killer));
@@ -122,7 +123,7 @@ namespace Schumix.GameAddon.MaffiaGames
 						list.TryGetValue(number, out name);
 						_detectivelist.Add(name.ToLower(), name);
 
-						if(Adminflag(name.ToLower()) == 2)
+						if((AdminFlag)Adminflag(name.ToLower()) == AdminFlag.Administrator)
 							_playerflist.Add(name.ToLower(), new Player(Rank.Detective, true));
 						else
 							_playerflist.Add(name.ToLower(), new Player(Rank.Detective));
@@ -150,7 +151,7 @@ namespace Schumix.GameAddon.MaffiaGames
 						list.TryGetValue(number, out name);
 						_doctorlist.Add(name.ToLower(), name);
 
-						if(Adminflag(name.ToLower()) == 2)
+						if((AdminFlag)Adminflag(name.ToLower()) == AdminFlag.Administrator)
 							_playerflist.Add(name.ToLower(), new Player(Rank.Doctor, true));
 						else
 							_playerflist.Add(name.ToLower(), new Player(Rank.Doctor));
@@ -168,7 +169,7 @@ namespace Schumix.GameAddon.MaffiaGames
 					{
 						_normallist.Add(llist.Value.ToLower(), llist.Value);
 
-						if(Adminflag(llist.Value.ToLower()) == 2)
+						if((AdminFlag)Adminflag(llist.Value.ToLower()) == AdminFlag.Administrator)
 							_playerflist.Add(llist.Value.ToLower(), new Player(Rank.Normal, true));
 						else
 							_playerflist.Add(llist.Value.ToLower(), new Player(Rank.Normal));
