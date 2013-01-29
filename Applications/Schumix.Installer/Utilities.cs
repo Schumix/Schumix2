e2db29de79100d64f9b46951fce1d2b00164551f
Megax <megax@yeahunter.hu>
2013-01-29 20:46:30 +0100

    * Installer fájlai átkonvertálva és pár rész felfrissítve.

--- Applications/Schumix.Installer/Utilities.cs
+++ Applications/Schumix.Installer/Utilities.cs
@@ -30,38 +30,53 @@ namespace Schumix.Installer
 {
 	sealed class Utilities
 	{
+		private readonly object WriteLock = new object();
 		private Utilities() {}
 
 		public string GetUrl(string url)
 		{
-			using(var client = new WebClient())
+			lock(WriteLock)
 			{
-				client.Headers.Add("referer", Consts.SchumixReferer);
-				client.Headers.Add("user-agent", Consts.SchumixUserAgent);
-				client.Encoding = Encoding.UTF8;
-				return client.DownloadString(url);
+				return GetUrl(url, string.Empty, string.Empty);
 			}
 		}
-
+		
 		public string GetUrl(string url, string args)
 		{
-			using(var client = new WebClient())
+			lock(WriteLock)
 			{
-				client.Headers.Add("referer", Consts.SchumixReferer);
-				client.Headers.Add("user-agent", Consts.SchumixUserAgent);
-				client.Encoding = Encoding.UTF8;
-				return client.DownloadString(new Uri(url + HttpUtility.UrlEncode(args)));
+				return GetUrl(url, args, string.Empty);
 			}
 		}
-
+		
 		public string GetUrl(string url, string args, string noencode)
 		{
-			using(var client = new WebClient())
+			lock(WriteLock)
 			{
-				client.Headers.Add("referer", Consts.SchumixReferer);
-				client.Headers.Add("user-agent", Consts.SchumixUserAgent);
-				client.Encoding = Encoding.UTF8;
-				return client.DownloadString(new Uri(url + HttpUtility.UrlEncode(args) + noencode));
+				if(args != string.Empty && noencode == string.Empty)
+					url = url + HttpUtility.UrlEncode(args);
+				else if(args != string.Empty && noencode != string.Empty)
+					url = url + HttpUtility.UrlEncode(args) + noencode;
+				
+				var request = (HttpWebRequest)WebRequest.Create(url);
+				request.AllowAutoRedirect = true;
+				request.UserAgent = Consts.SchumixUserAgent;
+				request.Referer = Consts.SchumixReferer;
+				
+				int length = 0;
+				byte[] buf = new byte[1024];
+				var sb = new StringBuilder();
+				var response = (HttpWebResponse)request.GetResponse();
+				var stream = response.GetResponseStream();
+				
+				while((length = stream.Read(buf, 0, buf.Length)) != 0)
+				{
+					buf = Encoding.Convert(Encoding.GetEncoding(response.CharacterSet), Encoding.UTF8, buf);
+					sb.Append(Encoding.UTF8.GetString(buf, 0, length));
+				}
+				
+				response.Close();
+				return sb.ToString();
 			}
 		}
 
