fb61e5645ff3244c9ca0c7b85cf961dc7fba1a51
Megax <megax@yeahunter.hu>
2013-02-05 15:09:31 +0100

    * Lemaradt az sql fájl.

---
+++ Sql/Updates/3.9.8_base.sql
@@ -0,0 +1,5 @@
+-- huHU
+INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("huHU", "ThisIsNotAChannelDoNotWriteInPM", "Ez nem csatorna. Ne pm-ben írj!");
+
+-- enUS
+INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("enUS", "ThisIsNotAChannelDoNotWriteInPM", "This is not a channel. Do not write in PM!");
