f425201937523226e82c059fbad0db3c4dbd783a
Megax <megax@yeahunter.hu>
2013-05-11 17:10:46 +0200

    * build.sh javítva.

--- build.sh
+++ build.sh
@@ -5,17 +5,25 @@ git submodule update --init --recursive
 build_conf="$(echo "$1" | tr [:upper:] [:lower:])"
 
 if [ "$2" = "" ]; then
-	build_platform="AnyCPU"
+	build_platform=""
 else
 	build_platform="$(echo "$2" | tr [:upper:] [:lower:])"
 fi
 
 if [ "$1" = "" ]; then
-	xbuild /p:Configuration="Debug" /p:Platform=$build_platform Schumix.sln
+	xbuild /p:Configuration="Debug" Schumix.sln
 else
-	if [ $build_conf = "release" ]; then
-		xbuild /p:Configuration="Release" /p:Platform=$build_platform Schumix.sln
+	if [ "$build_platform" = "" ]; then
+		if [ $build_conf = "release" ]; then
+			xbuild /p:Configuration="Release" Schumix.sln
+		else
+			xbuild /p:Configuration="Debug" Schumix.sln
+		fi
 	else
-		xbuild /p:Configuration="Debug" /p:Platform=$build_platform Schumix.sln
+		if [ $build_conf = "release" ]; then
+			xbuild /p:Configuration="Release" /p:Platform=$build_platform Schumix.sln
+		else
+			xbuild /p:Configuration="Debug" /p:Platform=$build_platform Schumix.sln
+		fi
 	fi
 fi
