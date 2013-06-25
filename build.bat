f1cac1eefed3ba8dee77e7a2c5de6c5cdcd7dd8d
Megax <megax@yeahunter.hu>
2013-06-25 19:53:33 +0200

    * #86-os issue elkeszult.

--- build.bat
+++ build.bat
@@ -1,1 +1,17 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Schumix.sln /p:Configuration=Release /p:PlatformTarget=x86
+@echo off
+SETLOCAL
+SET BUILD_CONFIG=%1
+
+if "%BUILD_CONFIG%" == "" (
+	set BUILD_CONFIG=Release
+)
+
+IF %BUILD_CONFIG%==Debug (
+	%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Schumix.sln /p:Configuration=Debug /p:PlatformTarget=x86 /flp:LogFile=msbuild.log;Verbosity=Detailed
+)
+
+IF %BUILD_CONFIG%==Release (
+	%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Schumix.sln /p:Configuration=Release /p:PlatformTarget=x86 /flp:LogFile=msbuild.log;Verbosity=Detailed
+)
+
+ENDLOCAL
