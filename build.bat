3ea3a3c19fe06bdbcafdbb3adbb504271574a4f7
Megax <megax@yeahunter.hu>
2013-06-25 19:57:20 +0200

    * Kis egyszerusites a majdani modositashoz.

--- build.bat
+++ build.bat
@@ -1,17 +1,18 @@
 @echo off
 SETLOCAL
 SET BUILD_CONFIG=%1
+SET MSBUILD_EXE=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 
 if "%BUILD_CONFIG%" == "" (
 	set BUILD_CONFIG=Release
 )
 
 IF %BUILD_CONFIG%==Debug (
-	%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Schumix.sln /p:Configuration=Debug /p:PlatformTarget=x86 /flp:LogFile=msbuild.log;Verbosity=Detailed
+	%MSBUILD_EXE% Schumix.sln /p:Configuration=Debug /p:PlatformTarget=x86 /flp:LogFile=msbuild.log;Verbosity=Detailed
 )
 
 IF %BUILD_CONFIG%==Release (
-	%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Schumix.sln /p:Configuration=Release /p:PlatformTarget=x86 /flp:LogFile=msbuild.log;Verbosity=Detailed
+	%MSBUILD_EXE% Schumix.sln /p:Configuration=Release /p:PlatformTarget=x86 /flp:LogFile=msbuild.log;Verbosity=Detailed
 )
 
 ENDLOCAL
