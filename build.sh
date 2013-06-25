553b150862bc41d0ccb58b0f6c43f6a1d33c4be6
Megax <megax@yeahunter.hu>
2013-06-25 19:27:35 +0200

    * build.sh-hoz hozzáadva a fordítás logolása.

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ if [ "$1" = "" ]; then
 else
 	if [ "$build_platform" = "" ]; then
 		if [ $build_conf = "release" ]; then
-			xbuild /p:Configuration="Release" Schumix.sln
+			xbuild /p:Configuration="Release" Schumix.sln /flp:LogFile=xbuild.log
 		else
-			xbuild /p:Configuration="Debug" Schumix.sln
+			xbuild /p:Configuration="Debug" Schumix.sln /flp:LogFile=xbuild.log
 		fi
 	else
 		if [ $build_conf = "release" ]; then
-			xbuild /p:Configuration="Release" /p:PlatformTarget=$build_platform Schumix.sln
+			xbuild /p:Configuration="Release" /p:PlatformTarget=$build_platform Schumix.sln /flp:LogFile=xbuild.log
 		else
-			xbuild /p:Configuration="Debug" /p:PlatformTarget=$build_platform Schumix.sln
+			xbuild /p:Configuration="Debug" /p:PlatformTarget=$build_platform Schumix.sln /flp:LogFile=xbuild.log
 		fi
 	fi
 fi
