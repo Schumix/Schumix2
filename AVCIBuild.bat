ec79219b34759d655478563884c4ed9349ea2275
Megax <megax@yeahunter.hu>
2013-12-24 12:10:26 +0100

    * Kicsit megreformálva mert még mindig hibás.

--- AVCIBuild.bat
+++ AVCIBuild.bat
@@ -6,8 +6,40 @@ SET MSBUILD_EXE=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 :: git submodule update --init --recursive
 :: Rossz a recursive megoldás ezért van így kézileg megoldva. Az a baj hogy nincs elég jogosultság a működtetéséhez.
 git submodule update --init
-git submodule foreach git submodule update --init
-git submodule foreach git submodule foreach git submodule update --init
+
+:: NLua/KeraLua
+cd External\NLua\Core\KeraLua
+git submodule update --init
+
+:: NLua/KeraLua/lua
+cd external\lua
+git submodule update --init
+
+:: NLua/KeraLua/LuaTests
+cd ..\..
+cd tests\LuaTests
+git submodule update --init
+
+:: NLua/KopiLua
+cd ..\..\..
+cd KopiLua
+git submodule update --init
+
+:: NLua/KopiLua/LuaTests
+cd tests\LuaTests
+git submodule update --init
+
+:: NLua/LuaTests
+cd ..\..\..\..
+cd tests\LuaTests
+git submodule update --init
+
+:: NLua/LuaTests
+cd ..\..\..
+cd ngit\External\SharpZipLib
+git submodule update --init
+
+cd ..\..\..\..
 
 if "%BUILD_CONFIG%" == "" (
 	set BUILD_CONFIG=Release
