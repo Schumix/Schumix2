ec33efc367ac049173e576f1ecd3818e6b2ae767
Megax <megax@yeahunter.hu>
2013-12-24 11:03:11 +0100

    * msbuild hazsnálata frissítve.

--- AVCIBuild.bat
+++ AVCIBuild.bat
@@ -1,7 +1,6 @@
 @echo off
 SETLOCAL
 SET BUILD_CONFIG=%1
-SET MSBUILD_EXE=msbuild
 
 git submodule init
 git submodule update
@@ -11,11 +10,11 @@ if "%BUILD_CONFIG%" == "" (
 )
 
 IF %BUILD_CONFIG%==Debug (
-	%MSBUILD_EXE% Schumix.sln /p:Configuration=Debug /flp:LogFile=msbuild.log;Verbosity=Detailed
+	msbuild Schumix.sln /p:Configuration=Debug /flp:LogFile=msbuild.log;Verbosity=Detailed
 )
 
 IF %BUILD_CONFIG%==Release (
-	%MSBUILD_EXE% Schumix.sln /p:Configuration=Release /flp:LogFile=msbuild.log;Verbosity=Detailed
+	msbuild Schumix.sln /p:Configuration=Release /flp:LogFile=msbuild.log;Verbosity=Detailed
 )
 
 ENDLOCAL
