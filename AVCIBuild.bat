7014fed86cf208bb6df85d34edd3c60ccded833d
Megax <megax@yeahunter.hu>
2013-12-24 11:49:38 +0100

    * Elvileg most már tökéletes lesz.

--- AVCIBuild.bat
+++ AVCIBuild.bat
@@ -3,7 +3,11 @@ SETLOCAL
 SET BUILD_CONFIG=%1
 SET MSBUILD_EXE=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 
+:: git submodule update --init --recursive
+:: Rossz a recursive megoldás ezért van így kézileg megoldva. Az a baj hogy nincs elég jogosultság a működtetéséhez.
 git submodule update --init
+git submodule foreach git submodule update --init
+git submodule foreach git submodule foreach git submodule update --init
 
 if "%BUILD_CONFIG%" == "" (
 	set BUILD_CONFIG=Release
