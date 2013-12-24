e2b641223fb62db63713a4b3b820ca7256ed40d8
Megax <megax@yeahunter.hu>
2013-12-24 12:44:13 +0100

    * Teszt...

--- AVCIBuild.bat
+++ AVCIBuild.bat
@@ -1,32 +1,33 @@
 @echo off
+git submodule update --init --recursive
+
 SETLOCAL
 SET BUILD_CONFIG=%1
 SET MSBUILD_EXE=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 
-:: git submodule update --init --recursive
 :: Rossz a recursive megoldás ezért van így kézileg megoldva. Az a baj hogy nincs elég jogosultság a működtetéséhez.
-git submodule update --init
+::git submodule update --init
 
 :: NLua
-cd External\NLua
-git submodule update --init
+::cd External\NLua
+::git submodule update --init
 
 :: NLua/KeraLua
-cd Core\KeraLua
-git submodule update --init
+::cd Core\KeraLua
+::git submodule update --init
 
 :: NLua/KopiLua
-cd ..
-cd KopiLua
-git submodule update --init
+::cd ..
+::cd KopiLua
+::git submodule update --init
 
 :: ngit
-cd ..\..
-cd ngit
-git submodule update --init
+::cd ..\..
+::cd ngit
+::git submodule update --init
 
 :: Schumix2 dir
-cd ..\..
+::cd ..\..
 
 if "%BUILD_CONFIG%" == "" (
 	set BUILD_CONFIG=Release
