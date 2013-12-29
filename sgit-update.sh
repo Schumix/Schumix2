18749c8d42eff1ce78b3b6f86ff1956fa7bc94cf
Megax <megax@yeahunter.hu>
2013-12-29 10:02:15 +0100

    * A sgit-update könyvtárszerkezete egyszerűsítve.

--- sgit-update.sh
+++ sgit-update.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+Dependencies="../../../../Dependencies/SGit"
+
 git submodule update --init --recursive
 
 cd External/SGit
 ./build.sh
 cd Run/Release
 
-if [ ! -e ../../../../Dependencies/SGit ]; then
-	mkdir ../../../../Dependencies/SGit
+if [ ! -e $Dependencies ]; then
+	mkdir $Dependencies
 fi
 
 for file in *.dll
 do
 	echo $file;
-	cp $file ../../../../Dependencies/SGit/$file
+	cp $file $Dependencies/$file
 done
 
 echo "SGit.exe";
-cp SGit.exe ../../../../Dependencies/SGit/SGit.exe
+cp SGit.exe $Dependencies/SGit.exe
 echo "Done";
