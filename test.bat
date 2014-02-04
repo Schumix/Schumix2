0e9c2295dc91d2fff2717b9120eab402a2df4869
Megax <megax@yeahunter.hu>
2014-02-04 17:39:41 +0100

    * Fix.

--- test.bat
+++ test.bat
@@ -3,6 +3,6 @@ SETLOCAL
 msbuild Schumix.sln /p:Configuration=Debug /flp:LogFile=msbuild.log;Verbosity=Detailed
 
 cd Run\Tests\Debug
-..\..\..\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe Schumix.Framework.Test.dll /noshadow /result:shippable\testresults\testresults.xml
-..\..\..\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe Schumix.Irc.Test.dll /noshadow /result:shippable\testresults\testresults.xml
+.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Framework.Test.dll /noshadow /result:shippable\testresults\testresults.xml
+.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Irc.Test.dll /noshadow /result:shippable\testresults\testresults.xml
 ENDLOCAL
