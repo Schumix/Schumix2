ce3b3530eaf49b168790cabaf710fcd16dc53bc6
Megax <megax@yeahunter.hu>
2014-02-04 17:42:20 +0100

    * Fix3.

---
+++ test.cmd
@@ -0,0 +1,2 @@
+.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Framework.Test.dll /noshadow /result:shippable\testresults\testresults.xml
+.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Irc.Test.dll /noshadow /result:shippable\testresults\testresults.xml
