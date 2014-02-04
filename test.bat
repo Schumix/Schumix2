9eb73f5302f0304e0aa4039c91a3a1f0f7ab6db3
Megax <megax@yeahunter.hu>
2014-02-04 18:17:18 +0100

    * Test.bat törölve.

--- test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SETLOCAL
-msbuild Schumix.sln /p:Configuration=Debug /flp:LogFile=msbuild.log;Verbosity=Detailed
-
-.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Framework.Test.dll /noshadow /result:shippable\testresults\testresults.xml
-.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Irc.Test.dll /noshadow /result:shippable\testresults\testresults.xml
-ENDLOCAL
