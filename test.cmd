208fc535ca0499434e291400fbd462cf2fb88151
Megax <megax@yeahunter.hu>
2014-02-04 18:09:11 +0100

    * Utolsó probálkozás...

--- test.cmd
+++ test.cmd
@@ -1,2 +1,2 @@
-.\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Framework.Test.dll /noshadow /result:shippable\testresults\testresults.xml
-.\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe .\Run\Tests\Debug\Schumix.Irc.Test.dll /noshadow /result:shippable\testresults\testresults.xml
+.\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe Run\Tests\Debug\Schumix.Framework.Test.dll /noshadow /result:shippable\testresults\testresults.xml
+.\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe Run\Tests\Debug\Schumix.Irc.Test.dll /noshadow /result:shippable\testresults\testresults.xml
