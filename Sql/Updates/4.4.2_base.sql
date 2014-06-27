6e168104e1d4871afc5f541fee54f2fd48c13286
Megax <megax@yeahunter.hu>
2014-06-27 21:35:40 +0200

    * resetsubmodule.sh hozzáadva a forráshoz.

---
+++ Sql/Updates/4.4.2_base.sql
@@ -0,0 +1,5 @@
+-- huHU
+INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("huHU", "ThisIsntInTheWebsiteList", "Nem szerepel a website listán!");
+
+-- enUS
+INSERT INTO `localized_warning` (`Language`, `Command`, `Text`) VALUES ("enUS", "ThisIsntInTheWebsiteList", "This isn't in the website list!");
