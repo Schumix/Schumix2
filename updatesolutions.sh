3c2746b0f24d0cd1843342f1fbc61a0aabedaeae
Megax <megax@yeahunter.hu>
2014-07-04 13:26:46 +0200

    * Nyelvi fájl frissítve. Po.csproj-hoz is feltöltve az automatikus fájlgenerálás lehetősége.

--- updatesolutions.sh
+++ updatesolutions.sh
@@ -2,6 +2,7 @@
 
 main () {
   schumixvs_sln_update
+  po_csproj_update
 }
 
 schumixvs_sln_update () {
@@ -9,10 +10,23 @@ schumixvs_sln_update () {
   echo "Update $f"
 
   if [ -e $f ]; then
-    rm SchumixVS.sln
+    rm $f
   fi
 
   cp Schumix.sln $f
+
+  find $f -type f -exec sed -i 's/Po.mdproj/Po.csproj/g' {} \;
+}
+
+po_csproj_update () {
+  f=Po/Po.csproj
+  echo "Update $f"
+
+  if [ -e $f ]; then
+    rm $f
+  fi
+
+  cp Po/Po.mdproj $f
 
   find $f -type f -exec sed -i 's/Po.mdproj/Po.csproj/g' {} \;
 }
